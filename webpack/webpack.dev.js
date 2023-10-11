dff6dd04ef2f28216917c8edb62641579ea8a8d7
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-10-11 14:35:35 -0700

    chore: refactored scripts +script now points at a src file for webpack

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,10 +1,17 @@
 const { merge } = require('webpack-merge');
 const webpackConfig = require('./webpack.config');
+const path = require('path');
 
 module.exports = merge(webpackConfig, {
     mode: 'development',
+    //Unsure of what to do with source-map
+    // devtool: 'inline-source-map',
     entry: './src/index.tsx',
     devServer: {
+        contentBase: path,
         setupExitSignals: true,
     },
+    optimization: {
+        minimize: false
+    }
 });
