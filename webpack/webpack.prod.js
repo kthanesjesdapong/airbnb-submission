2a7c57d14b164af48d267cd10f67ab9f743b1d35
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-10-11 16:02:24 -0700

    chore: add plugins for prod configs + add terserplugin and comment out split chunks

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,3 +1,4 @@
+const TerserPlugin = require('terser-webpack-plugin');
 const { merge } = require('webpack-merge');
 const webpackConfig = require('./webpack.config');
 
@@ -6,16 +7,16 @@ module.exports = merge(webpackConfig, {
     optimization: {
         minimizer: [new TerserPlugin()],
     },
-    splitChunks: {
-        cacheGroups: {
-            vendors: {
-                priority: -10,
-                test: /[\\/]node_modules[\\/]/
-            }
-        },
-        chunks: 'async',
-        minChunks: 1,
-        minSize: 30000,
-        name: false
-    }
+    // splitChunks: {
+    //     cacheGroups: {
+    //         vendors: {
+    //             priority: -10,
+    //             test: /[\\/]node_modules[\\/]/
+    //         }
+    //     },
+    //     chunks: 'async',
+    //     minChunks: 1,
+    //     minSize: 30000,
+    //     name: false
+    // }
 });
