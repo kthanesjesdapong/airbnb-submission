44b68598b2cb0012cf3df760c1c39eeadd245836
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-10-18 18:22:29 -0700

    chore: set-up applayout structure

---
+++ src/app/App.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+
+const AppContainer = styled.div`
+  
+`;
+
+export { AppContainer };
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,21 @@
 // Root LayOut
-// import { Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { AppContainer } from './App.styled';
 
 const App = () => {
     return (
-        <div>COMING IN FROM APP dsdslol lol dsdsdstestdsa
-        </div>
+        <AppContainer>
+            <header>
+                nav goes here
+            </header>
+            <main>
+                <Outlet />
+                main content goes here
+            </main>
+            <footer>
+                footer goes here
+            </footer>
+        </AppContainer>
     );
 };
 
