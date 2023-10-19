3f32b6fb144e3ae77a1c5244b3308671f63cfd5e
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-10-18 20:47:22 -0700

    chore: init client folder

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -3,14 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { StyledThemeProvider } from "./providers/Style-Theme-Provider";
 import { RouterProvider } from "react-router-dom";
 import { router } from "@shared/router";
-// import App from '@app/App';
 
 //Render Providers here
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <StrictMode>
         <StyledThemeProvider>
-            {/* <App /> */}
             <RouterProvider router={router} />
         </StyledThemeProvider>
     </StrictMode>
