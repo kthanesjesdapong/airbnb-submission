89a8bcb61db1d054a98a57be930b98a71da7a35a
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-10-24 14:05:29 -0700

    chore: storybook init

--- client/.storybook/main.ts
+++ client/.storybook/main.ts
@@ -1,19 +1,28 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-addons: [
-'@storybook/addon-links',
-'@storybook/addon-essentials',
-'@storybook/addon-onboarding',
-'@storybook/addon-interactions',
-],
-framework: {
-name: '@storybook/react-webpack5',
-options: {},
-},
-docs: {
-autodocs: 'tag',
-},
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    {
+      name: 'storybook-addon-swc',
+      options: {
+        enable: true,
+        enableSwcLoader: true,
+        enableSwcMinify: true,
+        swcLoaderOptions: {},
+        swcMinifyOptions: {},
+      },
+    },],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
 };
 export default config;
