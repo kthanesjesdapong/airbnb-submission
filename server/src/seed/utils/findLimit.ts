949f323066696b682b06297648ab2d806bfbcb4e
Kavin Thanesjesdapong <KavinThanesjesdapong@gmail.com>
2023-11-30 15:55:08 -0800

    fix: findLimit returns whole numbers only and will return 50,1 if n is <= 50, or if a prime number > 50 is return will return 50, ceil(n/50)

--- server/src/seed/utils/findLimit.ts
+++ server/src/seed/utils/findLimit.ts
@@ -1,14 +1,13 @@
-export const findLimit = (n: number): [number, number] | null => {
+export const findLimit = (n: number): [number, number] => {
+  if (n <= 50) return [50, 1];
   let highestFactorPair: [number, number] | null = null;
-
   for (let i = 1; i <= n / 2; i++) {
-    if (n % 2) {
+    if (n % i === 0) {
       const correspondingFactor = n / i;
       if (i < 50 && correspondingFactor < 50) {
         highestFactorPair = [i, correspondingFactor];
       }
     }
   }
-  return highestFactorPair;
+  return highestFactorPair ?? [50, Math.ceil(n / 50)];
 };
-
